e581422246acc9d39048611c77883fd50f68efc9
Jan Nicklas <j.nicklas@me.com>
2023-10-27 12:00:27 +0200

    formatting

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import puppeteer from 'puppeteer';
 import { gzipSize } from 'gzip-size';
 
 const url = process.argv[2] || "https://www.npmjs.com/";
+const json = process.argv[3] === "--json";
 
 (async () => {
     const browser = await puppeteer.launch({
@@ -23,35 +24,54 @@ const url = process.argv[2] || "https://www.npmjs.com/";
         },
     }
 
-    // Listen to network requests
-    page.on('response', async (response) => {
-        const url = response.url();
-        const type = response.request().resourceType();
+    const measurments: Promise<void>[] = [];
 
-        if (type === 'script') {
-            const content = (await response.text());
-            sizes.js.raw += content.length;
-            sizes.js.gzip += await gzipSize(content);
-            sizes.js.count++;
-        }
-        else if (type === 'stylesheet') {
-            const content = (await response.text());
-            sizes.css.raw += content.length;
-            sizes.css.gzip += await gzipSize(content);
-            sizes.css.count++;
-        }
+    // Listen to network requests
+    page.on('response', (response) => {
+        measurments.push((async () => {
+            const url = response.url();
+            const type = response.request().resourceType();
+    
+            if (type === 'script') {
+                const content = (await response.text());
+                sizes.js.raw += content.length;
+                sizes.js.gzip += await gzipSize(content);
+                sizes.js.count++;
+            }
+            else if (type === 'stylesheet') {
+                const content = (await response.text());
+                sizes.css.raw += content.length;
+                sizes.css.gzip += await gzipSize(content);
+                sizes.css.count++;
+            }   
+        })());
+       
     });
 
     await page.goto(url);
 
     await browser.close();
 
+    await Promise.all(measurments);
+
+    const formatNumber = (num: number) => {
+        return num.toLocaleString('en-US', {maximumFractionDigits: 2});
+    }
+
+    if (json) {
+        console.log(JSON.stringify(sizes, null, 2));
+        return;
+    }
+    console.log(``);
     console.log(`Stats for ${url}`);
+    console.log(``);
     console.log(`JavaScript download size (${sizes.js.count} file${sizes.js.count === 1 ? '' : 's'})`);
-    console.log(`Raw size: ${sizes.js.raw} bytes`);
-    console.log(`Gzip size: ${sizes.js.gzip} bytes`);
+    console.log(`Raw size: ${formatNumber(sizes.js.raw)} bytes`);
+    console.log(`Gzip size: ${formatNumber(sizes.js.gzip)} bytes`);
     console.log(``);
     console.log(`CSS download size (${sizes.css.count} file${sizes.css.count === 1 ? '' : 's'})`);
-    console.log(`Raw size: ${sizes.css.raw} bytes`);
-    console.log(`Gzip size: ${sizes.css.gzip} bytes`);
+    console.log(`Raw size: ${formatNumber(sizes.css.raw)} bytes`);
+    console.log(`Gzip size: ${formatNumber(sizes.css.gzip)} bytes`);
+    console.log(``);
+    
 })()
--- readme.md
+++ readme.md
@@ -8,11 +8,29 @@ Usage:
 npx @jantimon/url-asset-size https://www.github.com
 
 Stats for https://www.github.com
+
 JavaScript download size (50 files)
-Raw size: 1744226 bytes
-Gzip size: 191757 bytes
+Raw size: 1,744,226 bytes
+Gzip size: 278,621 bytes
 
 CSS download size (11 files)
-Raw size: 1114168 bytes
-Gzip size: 124438 bytes
+Raw size: 1,114,168 bytes
+Gzip size: 86,877 bytes
+
+```
+
+```
+npx @jantimon/url-asset-size https://www.github.com --json
+{
+  "js": {
+    "count": 50,
+    "raw": 1744226,
+    "gzip": 280056
+  },
+  "css": {
+    "count": 11,
+    "raw": 1114168,
+    "gzip": 134526
+  }
+}
 ```
